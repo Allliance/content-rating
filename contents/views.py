db074d84df5787e66746ad7a5b5d87bfb07c3dca
Allliance <alians310322@gmail.com>
2025-01-25 11:03:41 +0330

    Add content list view

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import OuterRef, Subquery
+from .models import Content, Rating
+from .serializers import ContentSerializer
 
-# Create your views here.
+class ContentListView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ContentSerializer
+    pagination_class = PageNumberPagination
+    
+    def get_queryset(self):
+        user_rating = Rating.objects.filter(
+            content=OuterRef('pk'),
+            user_id=self.request.user.id
+        ).values('rating')[:1]
+        
+        queryset = Content.objects.annotate(
+            user_rating=Subquery(user_rating)
+        ).select_related()
+        
+        return queryset
